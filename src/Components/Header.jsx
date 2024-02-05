9bd7e76f5bb8868779388e763cf32666545f9dd6
B0pp3d <ollikavs@gmail.com>
2024-02-05 16:26:56 +0000

    View All Articles with Homepage & Button + Routing

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <>
+        <div className="Header">
+            <header>
+                <h1>NC News Frontend</h1>
+            </header>
+        </div>
+        </>
+    )
+}
